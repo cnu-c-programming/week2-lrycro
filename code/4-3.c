26e16123a85dbc02e6285fc8f68da78a9d208079
Sewon Ahn <129852908+lrycro@users.noreply.github.com>
2026-03-13 13:42:51 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -2,7 +2,30 @@
 
 int main()
 {
+    int a;
+    int isPrime = 1;
 
+    scanf("%d", &a);
+
+    if (a == 1) {
+        isPrime = 0;
+    } else {
+        for (int i = 2; i * i <= a; i++) {
+            if (a % i == 0) {
+                isPrime = 0;
+            } else {
+                isPrime = 1;
+            }
+            break;
+        }
+    }
+
+    if (isPrime == 1) {
+        printf("true\n");
+    } else {
+        printf("false\n");
+    }
+    
     return 0;
 }
 
