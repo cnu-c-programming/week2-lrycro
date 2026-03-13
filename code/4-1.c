d143a7a436efeed1b452e20c9b65626579d59ffb
Sewon Ahn <129852908+lrycro@users.noreply.github.com>
2026-03-13 13:42:01 +0900

    Update 4-1.c

--- code/4-1.c
+++ code/4-1.c
@@ -2,7 +2,33 @@
 
 int main()
 {
+    int a, b;
+    char op;
 
+    scanf("%d %d %c", &a, &b, &op);
+
+    switch(op) {
+        case '+':
+            printf("%d\n", a + b);
+            break;
+        case '-':
+            printf("%d\n", a - b);
+            break;
+        case '*':
+            printf("%d\n", a * b);
+            break;
+        case '/':
+            if (b != 0) {
+                printf("%d\n", a / b);
+            }
+            else {
+                printf("Division by zero\n");
+            }
+            break;
+        default:
+            printf("Error\n");
+    }
+    
     return 0;
 }
 
